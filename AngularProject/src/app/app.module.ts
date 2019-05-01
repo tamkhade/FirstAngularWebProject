1dc399a2f82425c1df482e66489187ff665f5bc5
tamkhade21 <tamkhade21@gmail.com>
2019-05-01 15:12:42 +0530

    bug fixing

--- AngularProject/src/app/app.module.ts
+++ AngularProject/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NewCmpComponent } from './new-cmp/new-cmp.component';
+ import { NewCmpComponent } from './new-cmp/new-cmp.component';
 
 
 @NgModule({
